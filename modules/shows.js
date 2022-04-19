8fcb29774a338eed41b6e15848ab10e6a23696ee
aboongm <aboongm@yahoo.com>
2022-04-19 17:49:51 +0530

    Add fetchShows function to fetch tv shows from api

---
+++ modules/shows.js
@@ -0,0 +1,37 @@
+// const url = "https://api.tvmaze.com/shows/1/episodes";
+const url = 'https://api.tvmaze.com/shows';
+
+const fetchShows = async (url) => {
+  const response = await fetch(url);
+  const result = response.json();
+  return result;
+};
+
+const displayShows = async () => {
+  const showList = document.querySelector('.tvshow-list');
+  const shows = await fetchShows(url).then((result) => result);
+
+  if (shows.length !== 0) {
+    showList.innerHTML = '';
+    shows.forEach((element) => {
+      const showItem = `
+            <div class="showItem mx-5 my-2 d-flex flex-column align-items-center justify-content-center">
+                <img src="${element.image.medium}" alt="show">
+                <div class="d-flex align-items-center justify-content-between w-100">
+                    <h6 class="m-0 px-1 py-2 name">${element.name}</h6>
+                    <i class="fa-regular fa-heart icon px-1"></i>
+                </div>
+                <p class="m-0 px-1 pt-0 pb-1 likes w-100">Likes</p>
+                <h6 class="mx-0 mt-0 mb-1 px-2 py-1 border text-center name">Comments</h6>
+                </div>
+            `;
+      showList.insertAdjacentHTML('beforeend', showItem);
+    });
+  }
+};
+
+const postLikes = () => {
+  console.log('this is function for likes');
+};
+
+export { displayShows, postLikes };
