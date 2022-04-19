e6a964e109099b01d9600c7fb57183e65538cfb8
aboongm <aboongm@yahoo.com>
2022-04-18 17:46:44 -0700

    Choose the topic for our website and the TVMaze API.

---
+++ index.js
@@ -0,0 +1,13 @@
+const searchShow = () => {
+    const url = `https://api.tvmaze.com/shows/1/episodes`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const result = jsonData.map((element) => element);
+        console.log(result);
+      });
+  };
+  
+  searchShow();
+  
+  
